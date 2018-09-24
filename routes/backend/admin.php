73bf5d4b309c5c58a583def03eef07661adaaa7c
Liman Adamu Labaran <limanadamu@Limans-MacBook-Pro.local>
2018-09-24 12:47:08 +0000

    Add view for audit logs

--- routes/backend/admin.php
+++ routes/backend/admin.php
@@ -5,3 +5,6 @@
  */
 Route::redirect('/', '/admin/dashboard', 301);
 Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+
+Route::get('audits', 'AuditController@index')
+    ->middleware('auth', \App\Http\Middleware\AllowOnlyAdmin::class);
