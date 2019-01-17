3a90dbd3e5597509e530eddc851a5ede6a6ca4c3
Spomega <spomegasys@gmail.com>
2019-01-17 11:45:51 +0000

    added recommendation API

--- app/Http/Controllers/Api/RecommendationController.php
+++ app/Http/Controllers/Api/RecommendationController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Models\Auth\Crop;
+use App\Models\Auth\RecommendationCalculation;
+use App\Models\Auth\RecommendationActivity;
 use App\Http\Controllers\Controller;
 use JWTAuth;
 
@@ -13,11 +15,37 @@ class RecommendationController extends Controller
    {
        $user = JWTAuth::authenticate($request->token);
 
-       if($user){
+       $dataBuilder = array();
+       $recommendations = $crop->Recommendation()->get();
 
-          
-           return $crop->Recommendation()->get();
+       foreach($recommendations as $recommendation)
+       {
+           $responseBuilder = array(
+              'id' => $recommendation->id,
+               'crop_id'  =>$recommendation->crop_id,
+               'label'   => $recommendation->label_c,
+               'hierarchy' => $recommendation->hierarchy_c,
+               'condition' => $recommendation->condition_c,
+               'change_condition' => $recommendation->change_condition_c,
+               'change_option' => $recommendation->change_option_c,
+               'country'     => $recommendation->country_id,
+               'calculations' => RecommendationCalculation::where('recommendation_id',$recommendation->id)->get(),
+               'activity' => RecommendationActivity::where('recommendation_id',$recommendation->id)->get()
+           );
+
+
+           array_push($dataBuilder ,$responseBuilder);
        }
+
+
+
+     if($user)
+     {
+         return response()->json(['data' => $dataBuilder],200);
+
+     }else{
+         return response()->json(['error' => 'invalid_token'], 401);
+     }
 
    }
 
